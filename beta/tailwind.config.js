b76642756f944fec5449bd4cad06f68d72509a9c
Andreas Remdt <me@andreasremdt.com>
2024-05-16 22:04:21 +0200

    refactor: use radix ui for dialog

--- beta/tailwind.config.js
+++ beta/tailwind.config.js
@@ -21,10 +21,20 @@ export default {
         from: { height: "var(--radix-accordion-content-height)" },
         to: { height: "0px" },
       },
+      "overlay-show": {
+        from: { opacity: "0" },
+        to: { opacity: "1" },
+      },
+      "content-show": {
+        from: { opacity: "0", transform: "translate(-50%, -48%) scale(0.96)" },
+        to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+      },
     },
     animation: {
       "slide-down": "side-down 300ms cubic-bezier(0.87, 0, 0.13, 1)",
       "slide-up": "slide-up 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+      "overlay-show": "overlay-show 300ms cubic-bezier(0.16, 1, 0.3, 1)",
+      "content-show": "content-show 300ms cubic-bezier(0.16, 1, 0.3, 1)",
     },
   },
   plugins: [],
