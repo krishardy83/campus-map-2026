d7f9d6c77ab6c3ed772bca4203f136f94afff48a
Andreas Remdt <me@andreasremdt.com>
2024-05-22 15:52:32 +0200

    feat: handle 404, search ux

--- beta/src/components/search-form.tsx
+++ beta/src/components/search-form.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Combobox } from "@headlessui/react";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  Transition,
+} from "@headlessui/react";
 import clsx from "clsx";
 import data from "../data.json";
 import { getEntriesFromSearch } from "../lib/utils";
@@ -10,11 +16,17 @@ export default function SearchForm() {
   const [query, setQuery] = useState("");
   const results = getEntriesFromSearch(query, data);
 
+  function handleChange(value: string | null) {
+    if (value) {
+      navigate(`${value}?origin=search`);
+    }
+  }
+
   return (
     <search>
       <form className="px-6 py-4 relative">
-        <Combobox onChange={navigate}>
-          <Combobox.Input
+        <Combobox onChange={handleChange}>
+          <ComboboxInput
             type="search"
             placeholder="Search"
             aria-label="Search campus site"
@@ -22,31 +34,41 @@ export default function SearchForm() {
             value={query}
             onChange={(event) => setQuery(event.target.value)}
           />
-          <Combobox.Options className="absolute mt-1 bg-gray-50 rounded-md shadow-md left-6 right-6 top-16 z-10 max-h-96 overflow-y-auto">
-            {results.length === 0 && query !== "" ? (
-              <div className="px-4 py-2">
-                No entries match your search. Please try a different term.
-              </div>
-            ) : (
-              results.map((result) => (
-                <Combobox.Option
-                  key={result.entry_id}
-                  value={result.entry_id}
-                  className={({ active }) =>
-                    clsx(
-                      "px-4 py-2 block border-b border-gray-200 transition-colors hover:bg-sky-100 group cursor-pointer",
-                      { "ring-2 ring-sky-800 bg-sky-100": active }
-                    )
-                  }
-                >
-                  <span className="font-bold block group-hover:text-sky-800 transition-colors">
-                    {result.entry_title}
-                  </span>
-                  <span className="text-sm text-gray-500">{result.category_name}</span>
-                </Combobox.Option>
-              ))
-            )}
-          </Combobox.Options>
+          <Transition
+            enter="duration-200 ease-out"
+            enterFrom="scale-95 opacity-0"
+            enterTo="scale-100 opacity-100"
+            leave="duration-300 ease-out"
+            leaveFrom="scale-100 opacity-100"
+            leaveTo="scale-95 opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <ComboboxOptions className="absolute mt-1 bg-gray-50 rounded-md shadow-md left-6 right-6 top-16 z-10 max-h-96 overflow-y-auto">
+              {results.length === 0 && query !== "" ? (
+                <div className="px-4 py-2">
+                  No entries match your search. Please try a different term.
+                </div>
+              ) : (
+                results.map((result) => (
+                  <ComboboxOption
+                    key={result.entry_id}
+                    value={result.entry_id}
+                    className={({ focus }) =>
+                      clsx(
+                        "px-4 py-2 block border-b border-gray-200 transition-colors hover:bg-sky-100 group cursor-pointer",
+                        { "bg-sky-100": focus }
+                      )
+                    }
+                  >
+                    <span className="font-bold block group-hover:text-sky-800 transition-colors">
+                      {result.entry_title}
+                    </span>
+                    <span className="text-sm text-gray-500">{result.category_name}</span>
+                  </ComboboxOption>
+                ))
+              )}
+            </ComboboxOptions>
+          </Transition>
         </Combobox>
       </form>
     </search>
--- beta/src/routes/entry.tsx
+++ beta/src/routes/entry.tsx
@@ -1,15 +1,62 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { getCoverImageUrl, getEntryById, getGalleryImages, getGoogleMapsUrl } from "../lib/utils";
 import data from "../data.json";
 import Gallery from "../components/gallery";
 
 export default function Entry() {
   const { entryId } = useParams();
+  const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+
   const entry = getEntryById(data, entryId);
+  const fromSearch = searchParams.get("origin") === "search";
+
+  function handleNavigateBack() {
+    if (fromSearch) {
+      navigate("/");
+    } else {
+      navigate(-1);
+    }
+  }
 
   if (!entry) {
-    return <p>Entry not found</p>;
+    return (
+      <>
+        <img
+          src={getCoverImageUrl()}
+          alt=""
+          width="448"
+          height="298"
+          className="aspect-video object-cover bg-gray-50"
+        />
+
+        <div className="px-6 py-4">
+          <h1 className="text-2xl text-sky-800 font-bold mb-2">Not Found</h1>
+          <p>The entry you are looking for doesn't exist.</p>
+
+          <button
+            onClick={handleNavigateBack}
+            className="text-sky-800 uppercase font-bold text-sm mt-6 inline-flex items-center gap-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+            Back to overview
+          </button>
+        </div>
+      </>
+    );
   }
 
   return (
@@ -24,7 +71,7 @@ export default function Entry() {
 
       <nav className="border-b border-gray-200 py-4 px-6 flex justify-center">
         <button
-          onClick={() => navigate(-1)}
+          onClick={handleNavigateBack}
           className="inline-flex flex-col items-center focus-visible:outline-offset-0 outline-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-sky-800 text-sky-800 text-sm transition-colors hover:bg-sky-100 px-4 rounded-md py-1"
         >
           <svg
@@ -105,7 +152,7 @@ export default function Entry() {
         <p dangerouslySetInnerHTML={{ __html: entry.description }} />
 
         <button
-          onClick={() => navigate(-1)}
+          onClick={handleNavigateBack}
           className="text-sky-800 uppercase font-bold text-sm mt-6 inline-flex items-center gap-2"
         >
           <svg
