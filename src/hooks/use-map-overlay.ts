2e3f36901098958202f2c6ef8b6bddfc60ff096f
Andreas Remdt <me@andreasremdt.com>
2024-12-11 20:19:38 +0100

    refactor: remove legacy code

---
+++ src/hooks/use-map-overlay.ts
@@ -0,0 +1,54 @@
+import { useMapsLibrary } from "@vis.gl/react-google-maps";
+
+const BOUNDS: { [key: number]: [[number, number], [number, number]] } = {
+  15: [
+    [9375, 9377],
+    [12385, 12387],
+  ],
+  16: [
+    [18750, 18754],
+    [24771, 24774],
+  ],
+  17: [
+    [37501, 37509],
+    [49542, 49548],
+  ],
+  18: [
+    [75003, 75018],
+    [99085, 99097],
+  ],
+  19: [
+    [150007, 150037],
+    [198170, 198195],
+  ],
+  20: [
+    [300015, 300075],
+    [396340, 396390],
+  ],
+};
+
+export default function useMapOverlay() {
+  const maps = useMapsLibrary("maps");
+
+  if (maps) {
+    return new maps.ImageMapType({
+      getTileUrl(coords, zoom) {
+        if (
+          zoom < 15 ||
+          zoom > 20 ||
+          BOUNDS[zoom][0][0] > coords.x ||
+          coords.x > BOUNDS[zoom][0][1] ||
+          BOUNDS[zoom][1][0] > coords.y ||
+          coords.y > BOUNDS[zoom][1][1]
+        ) {
+          return null;
+        }
+
+        return `/tiles/${zoom}/tile_${coords.x}x${coords.y}-min.png`;
+      },
+      tileSize: new google.maps.Size(256, 256),
+    });
+  }
+
+  return null;
+}
---
+++ src/lib/utils.ts
@@ -0,0 +1,91 @@
+import type { Entry } from "../types";
+
+export function getCategories(entries: Entry[]): string[] {
+  return Array.from(
+    new Set<string>(
+      entries
+        .filter((entry) => entry.category_name.length > 0 && entry.live === "1")
+        .map((entry) => entry.category_name)
+    )
+  );
+}
+
+export function getEntriesForCategory(category: string, entries: Entry[]): Entry[] {
+  return entries.filter((entry) => entry.category_name === category && entry.live === "1");
+}
+
+export function getEntryByShortcut(entries: Entry[], shortcut?: string): Entry | undefined {
+  return entries.find((entry) => entry.shortcut === shortcut);
+}
+
+export function removeDuplicates(entries: Entry[]): Entry[] {
+  const selected: Entry[] = [];
+
+  entries.forEach((entry) => {
+    if (!selected.find((t) => t.entry_title === entry.entry_title)) {
+      selected.push(entry);
+    }
+  });
+
+  return selected;
+}
+
+export function getEntriesFromSearch(search: string, entries: Entry[]) {
+  const searchable: (keyof Entry)[] = ["category_name", "keywords", "entry_title", "description"];
+  const query = search.toLowerCase().trim();
+
+  if (query.length === 0) {
+    return entries;
+  }
+
+  return removeDuplicates(
+    entries.filter((entry) => {
+      let match = false;
+
+      searchable.forEach((key) => {
+        if (entry[key].toLowerCase().includes(query)) {
+          match = true;
+        }
+      });
+
+      return match;
+    })
+  );
+}
+
+export function getEntriesInThisBuilding(entry: Entry, entries: Entry[]): Entry[] {
+  return entries.filter(({ parent_building }) => parent_building === entry.entry_id);
+}
+
+export function getCoverImageUrl(image?: string): string {
+  if (image) {
+    return `http://www.messiah.edu/images/${image}`;
+  }
+
+  return "/images/location-image-placeholder.png";
+}
+
+export function getGoogleMapsUrl(coordinates: string): string {
+  return `https://maps.google.com?saddr=Current+Location&daddr=${coordinates}`;
+}
+
+export function getGalleryImages(entry: Entry): string[] {
+  const properties = [
+    "image_1_required",
+    "image_2_optional",
+    "image_3_optional",
+    "image_4_optional",
+  ] as const;
+
+  return properties.map((property) => entry[property]).filter((image) => image.length > 0);
+}
+
+export function setPageTitle(title?: string): void {
+  const base = "Messiah University Campus Map";
+
+  if (title) {
+    document.title = `${title} - ${base}`;
+  } else {
+    document.title = base;
+  }
+}
