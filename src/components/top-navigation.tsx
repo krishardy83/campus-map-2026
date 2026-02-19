fa693a3502b0e99964ffe3f8f1366b05a537d711
Kris Hardy <42982112+krishardy83@users.noreply.github.com>
2026-02-19 08:49:12 -0500

    Changed the request info link at the top

--- src/components/top-navigation.tsx
+++ src/components/top-navigation.tsx
@@ -2,7 +2,7 @@ export default function TopNavigation() {
   return (
     <nav className="fixed top-0 right-14 md:right-48 flex gap-2 z-10">
       <a
-        href="https://www.messiah.edu/homepage/1553/request_information"
+        href="https://www.messiah.edu/request-info/"
         target="_blank"
         rel="noopener nofollow noreferrer"
         className="bg-calypso-800 text-white px-3 text-sm py-1 rounded-b-sm hover:underline focus-visible:underline outline-hidden"
